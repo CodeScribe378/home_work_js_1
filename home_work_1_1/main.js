e9253578b9e156a36060a346fff79aba2d1f3068
CodeScribe378 <kristinahor91@gmail.com>
2025-02-05 00:06:27 +0100

    I did some changes in index.html and main.js

--- home_work_1_1/main.js
+++ home_work_1_1/main.js
@@ -1,23 +1,46 @@
+let greeting ="'hello'";
+let a = "'owu'";
+let b = "'com'";
+let c = "'ua'";
+let n1 = 1;
+let n2 = 10;
+let d = '1';
+let e = '10';
+let n3 = -999;
+let f ='-999';
+let n4 = 123;
+let g = '123';
+let n5 = 3.14;
+let h='3.14';
+let n6 = 2.7;
+let i = '2.7';
+let j = '16';
+let n7 = 16;
+let k = 'true';
+let l = 'false';
+
+
+
 console.log(greeting);
 console.log(a);
 console.log(b);
 console.log(c);
-console.log(n1); //number
-console.log(n2); //number
-console.log(d); //string
-console.log(e); //string
-console.log(n3); //number
-console.log(f); //string
-console.log(n4); //number
-console.log(g); //string
-console.log(n5); //number
-console.log(h); //string
-console.log(n6); //number
-console.log(i); //string
-console.log(j); //string
-console.log(n7); //number
-console.log(k); //string
-console.log(l); //string
+console.log(n1);
+console.log(n2);
+console.log(d);
+console.log(e);
+console.log(n3);
+console.log(f);
+console.log(n4);
+console.log(g);
+console.log(n5);
+console.log(h);
+console.log(n6);
+console.log(i);
+console.log(j);
+console.log(n7);
+console.log(k);
+console.log(l);
 console.log(n1<n2); //boolean
 console.log(n1>n2); //boolean
 
